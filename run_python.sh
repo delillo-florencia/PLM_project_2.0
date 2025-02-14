f89c633a78fca140aed53c3854b7302e3f265305
delillo-florencia <delillo.florencia@gmail.com>
2025-02-14 16:20:24 +0000

    update

--- run_python.sh
+++ run_python.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=compute
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=16
 #SBATCH --time=20:00:00
 #SBATCH --job-name=get_reps_test
