cloud Ampere implementation, FAESM bugfix, FlashAttention integration, set medium matmul precision

src/training/get_reps.py
@@ -1,18 +1,27 @@
 import pytorch_lightning as pl
 from utils.pyl_utils import ProteinDataModule, ProteinReprModule
+import torch
+import os
+
+
+
+# GLOBAL ENV
+RANK = int(os.environ.get("LOCAL_RANK", 0))
+DEVICES = torch.cuda.device_count()
+torch.set_float32_matmul_precision("medium")
 
 
 
 # MASTER PARAMS
 model_type = "8M"
-csv_file = '../../data/raw/uniprot_data_500k_sampled_250.csv'
-hash_file = '../../data/raw/uniprot_data_500k_sampled_250.hash'
-output_dir = "../../data/outputs/faesm_teacher_reps/"
+csv_file = '/home/cpebiosustain_gmail_com/workspace/PLM_project_2.0/data/uniprot_data_500k_sampled_250.csv'
+hash_file = '/home/cpebiosustain_gmail_com/workspace/PLM_project_2.0/data/uniprot_data_500k_sampled_250.hash'
+output_dir = "/home/cpebiosustain_gmail_com/workspace/PLM_project_2.0/data/outputs/faesm_teacher_reps/"
 
 # SAMPLER PARAMS
 sampler_params = {
-    "num_replicas": 1,
-    "rank": 0,
+    "num_replicas": DEVICES,
+    "rank": RANK,
     "max_batch_size": None,
     "max_batch_tokens": 2200,
     "shuffle": False}
@@ -28,11 +37,13 @@ model_module = ProteinReprModule(param_size=model_type, output_dir=output_dir)
 
 # trainer
 trainer = pl.Trainer(
-    devices=sampler_params["num_replicas"],
+    devices=DEVICES,
     accelerator="gpu",
     logger=False,
+    precision="bf16-true",
+    strategy="ddp",
     max_epochs=1,
-    limit_test_batches=1) # ONLY ONE BATCH FOR TESTING !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    limit_test_batches=4) # ONLY ONE BATCH FOR TESTING !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 # run the inference as test
-trainer.test(model_module, dataloaders=data_module.dataloader())
+trainer.test(model_module, dataloaders=data_module.dataloader(), verbose=False)