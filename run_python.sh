e47c301632a7e9e92e6ee133edd5e1abf6c6d90e
delillo-florencia <delillo.florencia@gmail.com>
2025-02-19 20:47:55 +0000

    prep for online training

--- run_python.sh
+++ run_python.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-#SBATCH --partition=compute
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=4
 #SBATCH --time=20:00:00
-#SBATCH --job-name=get_reps_test
-#SBATCH --output=reps_output.txt
+#SBATCH --job-name=get_reps_token_size
+#SBATCH --output=reps_output_tokens.txt
+#SBATCH --gres=gpu:4              
+#SBATCH --mem=600G                
+#SBATCH --partition=compute
 
-#python src/training/get_reps.py
-torchrun --standalone --nnodes=1 --nproc_per_node=4 src/training/get_reps.py
+
+torchrun --standalone --nnodes=1 --nproc_per_node=4 src/training/get_reps_token_size.py
+
+
 
 hostname
+
