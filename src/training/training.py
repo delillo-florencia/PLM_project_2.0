cleaned masking logic, class self bugfix, high precision for matmul

src/training/training.py
@@ -38,7 +38,7 @@ class ProteinReprModule(pl.LightningModule):
         rep = self.forward(batch)
         return rep
 
-    def extract_masked_logits(logits, masked_pos):
+    def extract_masked_logits(self, logits, masked_pos):
         masked_logi = []
         for i, positions in enumerate(masked_pos):
             # Adjust positions to account for the <str> token by adding 1 to each position.
@@ -55,7 +55,7 @@ class ProteinReprModule(pl.LightningModule):
 
         #  masking 
         masked_results = mask_batch(batch, batch_idx, self.current_epoch)
-        masked_sequences, masked_pos = zip(*masked_results)
+        masked_batch, masked_pos = zip(*masked_results)
     
         #save masked sequences
         if self.save_masked_sequences:
@@ -63,10 +63,10 @@ class ProteinReprModule(pl.LightningModule):
             os.makedirs(masked_sequences_dir, exist_ok=True)
             
             with open(os.path.join(masked_sequences_dir, f"batch_{batch_idx}_masked_sequences.txt"), 'w') as f:
-                for seq in masked_sequences:
-                    f.write(seq + "\n")
+                for sample in masked_batch:
+                    f.write(sample.masked_seq + "\n")
         
-        masked_data = [(sample.seq_id, masked_seq) for sample, masked_seq in zip(batch, masked_sequences)]
+        masked_data = [(sample.seq_id, sample.masked_seq) for sample in masked_batch]
         _, _, batch_tokens = self.batch_converter(masked_data)
         masked_tokens = batch_tokens.to(self.device)  # Masked tokens for logits
         batch_lens = (masked_tokens != self.alphabet.padding_idx).sum(1)
@@ -158,7 +158,7 @@ RANK = int(os.environ.get("SLURM_PROCID", 0))
 WORLD_SIZE = int(os.environ.get("SLURM_NTASKS", 1))
 LOCAL_RANK = int(os.environ.get("SLURM_LOCALID", 0))
 print("Init ok")
-torch.set_float32_matmul_precision("medium")
+torch.set_float32_matmul_precision("high")
 torch.cuda.set_device(LOCAL_RANK)
 DEVICES = torch.cuda.device_count()
 

src/utils/data_utils.py
@@ -3,7 +3,6 @@ import pickle
 import math
 import random
 import torch
-import numpy as np
 from torch.utils.data import Dataset, Sampler
 from faesm.esm import FAEsmForMaskedLM
 from esm import Alphabet
@@ -37,6 +36,10 @@ class Sequence:
         self.length = int(length)
         self.taxon_id = int(taxon_id)
         self.seq_id = str(seq_id)
+        self.masked_seq = ""
+
+    def add_masking(self, seq):
+        self.masked_seq = seq
 
 
 
@@ -98,7 +101,7 @@ class HashedProteinDataset(Dataset):
 
 class DynamicTaxonIdSampler(Sampler):
     def __init__(self, num_replicas, rank, seq_lengths, taxon_ids, num_buckets=128, min_len=0, max_len=1024,
-                 max_batch_tokens=None, max_batch_size=None, shuffle=False, shuffle_batch_order=True, seed=42, drop_last=False):
+                 max_batch_tokens=None, max_batch_size=None, shuffle=False, seed=42, drop_last=False):
         """
         A dynamic batch sampler supports DDP for robust training
         :param num_replicas: int
@@ -132,7 +135,6 @@ class DynamicTaxonIdSampler(Sampler):
         self.max_batch_tokens = max_batch_tokens or float('inf')
         self.max_batch_size = (max_batch_size + 1) if max_batch_size is not None else float('inf')
         self.shuffle = shuffle
-        self.shuffle_batch_order = shuffle_batch_order
         self.seed = seed
         self.drop_last = drop_last
         self.__epoch = 0
@@ -201,8 +203,6 @@ class DynamicTaxonIdSampler(Sampler):
             if bucket['indices']:
                 batches.append(bucket['indices'])
 
-        # make sure the number of butches works for DDP
-        random.seed(self.seed + self.__epoch)
         per_gpu = math.ceil(len(batches) / self.num_replicas)
         total = per_gpu * self.num_replicas
         dummy = total - len(batches)
@@ -210,13 +210,8 @@ class DynamicTaxonIdSampler(Sampler):
             batches += random.sample(batches, dummy)
         else:
             batches += [random.choice(batches) for _ in range(dummy)]
-
-        # shuffle batch order
-        if self.shuffle_batch_order:
-            rng = np.random.default_rng(self.seed + self.__epoch)
-            permuted_order = rng.permutation(len(batches))
-            batches = [batches[i] for i in permuted_order]
-
+        if self.shuffle:
+            random.shuffle(batches)
         self.__per_gpu_batch_num = per_gpu
         return batches
     