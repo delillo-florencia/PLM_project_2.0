618106044534f2d25e3044b0fea6bec00bae303b
delillo-florencia <delillo.florencia@gmail.com>
2025-04-17 10:39:28 +0000

    config fix

--- slurm_jobs/run_multinode_training.sh
+++ slurm_jobs/run_multinode_training.sh
@@ -9,9 +9,12 @@
 #SBATCH --output=training_output.log
 #SBATCH --error=training_error.log
 
+export MASTER_PORT=29500
+
 # launch one torchrun_slurm on each node via srun
 srun torchrun_slurm \
      --rdzv_backend=c10d \
      --rdzv_id=$SLURM_JOB_ID \
+     --rdzv_endpoint=${SLURM_JOB_NODELIST}:${MASTER_PORT} \
      /home/dtuteam/workspace/PLM_project_2.0/src/training/training_loop.py \
               --config /home/dtuteam/workspace/PLM_project_2.0/src/configs/config.yaml
