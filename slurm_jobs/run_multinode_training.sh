1e5d031954ff2857dfebdce229947a1439e366e3
delillo-florencia <delillo.florencia@gmail.com>
2025-04-17 10:51:02 +0000

    up

--- slurm_jobs/run_multinode_training.sh
+++ slurm_jobs/run_multinode_training.sh
@@ -9,12 +9,16 @@
 #SBATCH --output=training_output.log
 #SBATCH --error=training_error.log
 
+nodes=( $(scontrol show hostnames $SLURM_JOB_NODELIST) )
+export MASTER_ADDR=${nodes[0]}
 export MASTER_PORT=29500
 
 # launch one torchrun_slurm on each node via srun
 srun torchrun_slurm \
      --rdzv_backend=c10d \
      --rdzv_id=$SLURM_JOB_ID \
-     --rdzv_endpoint=${SLURM_JOB_NODELIST}:${MASTER_PORT} \
+     --rdzv_endpoint=${MASTER_ADDR}:${MASTER_PORT} \
+     --rdzv_id=${SLURM_JOB_ID} \
+     --rdzv_configs="join_timeout=1800,last_call_timeout=60" \
      /home/dtuteam/workspace/PLM_project_2.0/src/training/training_loop.py \
               --config /home/dtuteam/workspace/PLM_project_2.0/src/configs/config.yaml
