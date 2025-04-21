9cda0b0a75652efa43aa3c6951dedc478fc794ed
delillo-florencia <delillo.florencia@gmail.com>
2025-04-21 17:37:11 +0000

    a

--- slurm_jobs/run_multinode_training.sh
+++ slurm_jobs/run_multinode_training.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=plm_train
 #SBATCH --partition=g2
-#SBATCH --nodes=60                   
+#SBATCH --nodes=4                   
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
@@ -9,7 +9,7 @@
 #SBATCH --output=training_output.log
 #SBATCH --error=training_error.log
 
-#SBATCH --exclude=hpccluster-g2-ghpc-67
+#SBATCH --exclude=hpccluster-g2-ghpc-[67,69,27,45,52,26,0]
 
 MASTER_PORT=29540
 
@@ -18,7 +18,7 @@ read -r head_node <<< "$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n
 # 2) Extract the first external IP from that node
 read -r head_ip <<< "$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname -I | awk '{print $1}')"
 
-export OMP_NUM_THREADS=4
+export OMP_NUM_THREADS=1
 
 # Launch
 srun torchrun \
@@ -27,6 +27,7 @@ srun torchrun \
      --rdzv_backend=c10d \
      --rdzv_endpoint=${head_ip}:${MASTER_PORT} \
      --rdzv_id=${SLURM_JOB_ID} \
+     --rdzv_conf timeout=3600,heartbeat=1800 \
      /home/dtuteam/workspace/PLM_project_2.0/src/training/training_loop.py \
           --config /home/dtuteam/workspace/PLM_project_2.0/src/configs/config.yaml \
      
